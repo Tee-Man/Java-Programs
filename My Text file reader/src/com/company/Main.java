285a980d19b27a530b5cd6376a01a3a3e556c8fa
TeeTheCoder <teeman.systems@gmail.com>
2020-02-18 18:55:07 +0200

    Init commit

--- My Text file reader/src/com/company/Main.java
+++ My Text file reader/src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.err.println("\n\n====[ Program Start ]====\n");
+        try{
+            File myTxt = new File("src/Test.txt");
+            if(myTxt.exists()){
+                Scanner file = new Scanner(myTxt);
+
+                do {
+                    System.out.println(file.nextLine());
+                }while (file.hasNextLine());
+            }
+        }catch (Exception err){
+            System.out.println("\n===+ Program error +===");
+            System.err.println("\n\nError message: " + err.getMessage() +
+                                "\nError cause: " + err.getCause());
+            System.out.println("\n===+ Program error +===");
+        }
+
+        System.err.println("\n\n====[ Program End ]====\n");
+
     }
 }
